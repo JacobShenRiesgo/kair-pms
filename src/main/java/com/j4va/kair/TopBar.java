dbd50b04e1d926c58532602f577ae77228ad5ac9
JhakeJshane <jacobshen8888@gmail.com>
2025-11-04 18:56:11 +0800

    Top Bar V2

--- src/main/java/com/j4va/kair/TopBar.java
+++ src/main/java/com/j4va/kair/TopBar.java
@@ -1,25 +1,33 @@
 package com.j4va.kair;
 
 import javafx.application.Platform;
+import javafx.geometry.Bounds;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+
 
 public class TopBar{
     private static double xOffset = 0;
     private static double yOffset = 0;
-
     public static void setupTransparentStage(Stage stage, Scene scene, String title) {
         scene.setFill(Color.TRANSPARENT);
         stage.initStyle(StageStyle.TRANSPARENT);
         stage.setScene(scene);
         stage.setTitle(title);
+        Node root = scene.getRoot();
+        Rectangle clip = new Rectangle();
+        clip.setArcWidth(20);
+        clip.setArcHeight(20);
+        clip.widthProperty().bind(root.layoutBoundsProperty().map(Bounds::getWidth));
+        clip.heightProperty().bind(root.layoutBoundsProperty().map(Bounds::getHeight));
+        root.setClip(clip);
     }
-
     public static void enableWindowDragging(Stage stage, Node dragArea) {
         dragArea.setOnMousePressed((MouseEvent e) -> {
             xOffset = e.getSceneX();
@@ -67,6 +75,8 @@ public class TopBar{
 
             enableWindowDragging(stage, topBar);
             setupWindowButtons(stage, minimizeBtn, maximizeBtn, closeBtn);
+
+
         });
     }
 }
