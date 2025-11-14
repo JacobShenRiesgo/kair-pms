afc4639ab326fbe96ff3da5af05abe84bbd12342
JhakeJshane <jacobshen8888@gmail.com>
2025-11-14 22:38:53 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/resources/com/j4va/kair/taskcard.fxml

--- src/main/java/com/j4va/kair/TaskPopup.java
+++ src/main/java/com/j4va/kair/TaskPopup.java
@@ -31,8 +31,8 @@ public class TaskPopup {
         String title = titleField.getText();
         String desc = descriptionField.getText();
         String priority = priorityChoice.getValue();
-        TaskData task = new TaskData(title, desc, priority, "TODO");
         if (priority == null) priority = "None";
+        TaskData task = new TaskData(title, desc, priority, "TODO");
         if (callback != null) callback.accept(task);
 
         closePopup();
