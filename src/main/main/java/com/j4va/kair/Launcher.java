6f00e9730ef34869ef3d0a859b9e454e93dee490
JacobShenRiesgo <jsnriesgo@mymail.mapua.edu.ph>
2025-11-15 10:25:06 +0800

    Delete src/main/main directory

--- src/main/main/java/com/j4va/kair/Launcher.java
+++
@@ -1,7 +0,0 @@
-package com.j4va.kair;
-
-public class Launcher {
-    public static void main(String[] args) {
-        MainApplication.main(args);
-    }
-}
--- src/main/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.j4va.kair.kairprojectmanagementsystem {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-    requires javafx.graphics;
-
-    opens com.j4va.kair to javafx.fxml;
-    exports com.j4va.kair;
-}
